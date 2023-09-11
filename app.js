18e3d1ce2dc46e2abc803c20ba929e389b75dab2
HammadAli55 <hammadalii@outlook.com>
2023-09-11 10:27:06 +0500

    twilio api implementation

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const http = require('http');
+const twilio = require('twilio');
+const mongoose = require('mongoose');
+
+const app = express();
+const server = http.createServer(app);
+
+// Replace these values with your Twilio Account SID and Auth Token
+const TWILIO_ACCOUNT_SID = 'ACd934ed3718943b611bce68b71e7b274b';
+const TWILIO_AUTH_TOKEN = '3f158637454ecf3170ce30aeb363da10';
+
+// Replace these numbers with your main IVR number and the number to forward to
+const MAIN_IVR_NUMBER = '+17043135714';
+const FORWARD_TO_NUMBER = '+923365500513';
+
+// MongoDB connection URL and options
+const MONGODB_URL = 'mongodb+srv://hammad_shah:<password>@cluster0.mejjc0a.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection URL
+const MONGODB_OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGODB_URL, MONGODB_OPTIONS);
+
+// Define a schema for call logs
+const CallLogSchema = new mongoose.Schema({
+  caller: String,
+  callStatus: String,
+  userPressed: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const CallLog = mongoose.model('CallLog', CallLogSchema);
+
+// Create a Twilio client
+const client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+app.use(express.urlencoded({ extended: true }));
+
+// Handle incoming calls
+app.post('/voice', async (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  const userPressed = req.body.Digits;
+  const caller = req.body.From;
+
+  if (userPressed === '1') {
+    // Forward the call to another phone number
+    twiml.dial(FORWARD_TO_NUMBER);
+  } else if (userPressed === '2') {
+    // Allow the caller to leave a voicemail
+    twiml.say('Please leave a voicemail after the tone.');
+    twiml.record({
+      action: '/voicemail',
+      maxLength: 60, // Maximum voicemail length in seconds
+      playBeep: true,
+    });
+  } else {
+    // Invalid input, redirect to the main IVR menu
+    twiml.redirect('/ivr-menu');
+  }
+
+  // Log the call in the database
+  await CallLog.create({
+    caller,
+    callStatus: 'Received',
+    userPressed,
+  });
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// Handle voicemail recording
+app.post('/voicemail', async (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  twiml.say('Thank you for leaving a voicemail. Goodbye.');
+
+  // Log the voicemail in the database
+  await CallLog.create({
+    caller: req.body.From,
+    callStatus: 'Voicemail',
+  });
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// Main IVR menu
+app.post('/ivr-menu', (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  twiml.gather({
+    numDigits: 1,
+    action: '/voice',
+  }, (gatherNode) => {
+    gatherNode.say('Welcome to our IVR system. Press 1 to forward your call, or press 2 to leave a voicemail.');
+  });
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
